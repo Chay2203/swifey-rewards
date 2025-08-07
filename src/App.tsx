6ad57b4076f950a5beab4019b51a7451bc7e88bb
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2025-08-08 01:58:37 +0530

    dep fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import swifeyLogo from './assets/swifey-logo.svg'
-import splashWordMark from './assets/splashWordMark.png'
 
 function App() {
   const [isMobile, setIsMobile] = useState(false)
